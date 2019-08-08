4087843aa4505440e6d6d5f7d80ef953c5837423
choogoy <victormm0312@yandex.ru>
2019-08-08 22:50:21 +0400

    ex1

--- ex1.js
+++ ex1.js
@@ -1,14 +1,6 @@
-var money;
-var time;
-
-money = prompt ('Ваш бюджет на месяц?', 25000);
-time = prompt ('Введите дату в формате YYYY-MM-DD', '2019-08-08');
-
-var timeData = time;
-document.write(money);
-document.write(time);
-
-var expenses, optionalExpenses, income;
+var money = prompt ('Ваш бюджет на месяц?', 25000);
+var time = prompt ('Введите дату в формате YYYY-MM-DD', '2019-08-08');
+var optionalExpenses;
 
 var appData = {
 	money,
@@ -18,12 +10,7 @@ var appData = {
 	income: {},
 	savings: false
 }
+appData.expenses[0] = prompt ('Введите обязательную статью расходов в этом месяце', 'Продукты');
+appData.expenses[1] = prompt ('Во сколько обойдется?', 15000);
 
-console.log(appData.expenses);
-
-push.appData.
-push.appData.expenses[0] = prompt ('Введите обязательную статью расходов в этом месяце', 'Продукты');
-
-push.expenses[1] = prompt ('Во сколько обойдется?', 15000);
-
-alert (money/expenses[1]);
+alert('бюджет на 1 день: ' + Math.round(Number(money)/30));
