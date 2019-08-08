782f673377e0dde49b0781a4ccdb030a6fb2244e
Victor <victormm0312@yandex.ru>
2019-08-08 20:16:00 +0400

    first commit

---
+++ ex1.js
@@ -0,0 +1,26 @@
+var money;
+var time;
+
+money = prompt ('Ваш бюджет на месяц?', 25000);
+time = prompt ('Введите дату в формате YYYY-MM-DD', '2019-08-08');
+
+var timeData = time;
+
+var expenses, optionalExpenses, income;
+
+var appData = {
+	money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses,
+	income: {},
+	savings: false
+}
+
+console.log(appData.expenses);
+
+push.appData.expenses[0] = prompt ('Введите обязательную статью расходов в этом месяце', 'Продукты');
+
+push.expenses[1] = prompt ('Во сколько обойдется?', 15000);
+
+alert (money/expenses[1]);
