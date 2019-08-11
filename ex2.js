8408f9c77639a2a6e14dfaa392848dddabb094fe
choogoy <victormm0312@yandex.ru>
2019-08-11 18:22:20 +0400

    ex2.v2

--- ex2.js
+++ ex2.js
@@ -9,10 +9,11 @@ let appData = {
 	timeData: time,
 	savings: false
 }
+let i = 0;
 
 let err = 0; //счетик количества неправильных вводов данных :)
 
-for (let i = 0; i < 2; i++) {
+/* for (i = 0; i < 2; i++) {
 
     let a = prompt('Введите обязательную статью расходов в этом месяце'),
         b = prompt('Во сколько обойдется?');
@@ -22,6 +23,22 @@ for (let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
     } else {
         i-=1;
+        err++;
+        alert('Вводите корректные данные! Количество ошибок: ' + err);
+        console.log('error: ' + err);
+    }
+} */
+
+while (i < 2) {
+
+    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+        b = prompt('Во сколько обойдется?');
+            
+     if ((typeof(a))==='string' && (isFinite(a)) === false && (isFinite(b)) === true && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+		appData.expenses[a] = b;
+		i++;
+    } else {
         err++;
         alert('Вводите корректные данные! Количество ошибок: ' + err);
         console.log('error: ' + err);
